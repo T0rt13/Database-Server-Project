b8f66bf7ed98887c46d487e17fca1a767d89e28e
bodiebice <bodiebice@gmail.com>
2025-05-07 19:12:45 -0500

    tested n ready

--- file_uploading.py
+++ file_uploading.py
@@ -1,52 +1,43 @@
 import os
 from dotenv import load_dotenv
 from azure.identity import DefaultAzureCredential
-from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient, Error
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 
 load_dotenv() 
 connectionString = os.environ['AZURE_STORAGE_CONNECTION_STRING']
 
-def query1_createfile(connectionString, filePath, fileName):
+def query1_createfile(filePath, fileName):
     blob_service_client = BlobServiceClient.from_connection_string(connectionString)
-    container = blob_service_client.get_container_client("databases_project")
-    try:
-        with open(file=os.path.join(filePath, fileName), mode="rb") as data:
-            blob_client = container.upload_blob(name=fileName, data=data, overwrite=True)
-            if blob_client:
-                return True
-    except Error as e:
-        print(f"Error executing query: {e}")
-        return None
+    container = blob_service_client.get_container_client("databasesproject")
+    with open(file=os.path.join(filePath, fileName), mode="rb") as data:
+        blob_client = container.upload_blob(name=fileName, data=data, overwrite=True)
+        if blob_client:
+            return True
 
-def query3_getbloburl(connectionString, fileName):
+def query3_getbloburl(fileName):
     blob_service_client = BlobServiceClient.from_connection_string(connectionString)
-    blob_client = blob_service_client.get_blob_client(container="databases_project", blob=fileName)
-    try:
-        with open(file=os.path.join(r'filepath', 'filename'), mode="wb") as sample_blob:
-            download_stream = blob_client.download_blob()
-            sample_blob.write(download_stream.readall())
-    except Error as e:
-        print(f"Error executing query: {e}")
-        return None
+    blob_client = blob_service_client.get_blob_client(container="databasesproject", blob=fileName)
+    with open(file=os.path.join(r'', fileName), mode="wb") as sample_blob:
+        download_stream = blob_client.download_blob()
+        sample_blob.write(download_stream.readall())
 
-def query4_deleteblob(connectionString, fileName):
+def query4_deleteblob(fileName):
     blob_service_client = BlobServiceClient.from_connection_string(connectionString)
-    try:
-        blob_client = blob_service_client.get_blob_client(container="databases_project", blob=fileName)
-        blob_client.delete_blob()
-    except Error as e:
-        print(f"Error executing query: {e}")
-        return None
+    blob_client = blob_service_client.get_blob_client(container="databasesproject", blob=fileName)
+    blob_client.delete_blob()
 
-def query5_editblob(connectionString, currentFileName, filePath, fileName):
+def query5_editblob(currentFileName, filePath, fileName):
     blob_service_client = BlobServiceClient.from_connection_string(connectionString)
-    try:
-        blob_client = blob_service_client.get_blob_client(container="databases_project", blob=currentFileName)
-        blob_client.delete_blob()
-        blob_client = blob_service_client.get_blob_client(container="databases_project", blob=fileName)
-        with open(file=os.path.join(filePath, fileName), mode="rb") as data:
-            container = blob_service_client.get_container_client("databases_project")
-            blob_client = container.upload_blob(name=fileName, data=data, overwrite=True)
-    except Error as e:
-        print(f"Error executing query: {e}")
-        return None
+    blob_client = blob_service_client.get_blob_client(container="databasesproject", blob=currentFileName)
+    blob_client.delete_blob()
+    blob_client = blob_service_client.get_blob_client(container="databasesproject", blob=fileName)
+    with open(file=os.path.join(filePath, fileName), mode="rb") as data:
+        container = blob_service_client.get_container_client("databasesproject")
+        blob_client = container.upload_blob(name=fileName, data=data, overwrite=True)
+
+if __name__ == "__main__":
+    filePath = ""
+    fileName = "testfile.txt"
+    newFileName = "testfile2.txt"
+    query3_getbloburl(newFileName)
+
