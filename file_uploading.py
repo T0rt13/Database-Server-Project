cf4d045068e3ffa28da2829d9776de3d5e647052
bodiebice <bodiebice@gmail.com>
2025-05-07 19:54:21 -0500

    changes :D

--- file_uploading.py
+++ file_uploading.py
@@ -6,38 +6,31 @@ from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 load_dotenv() 
 connectionString = os.environ['AZURE_STORAGE_CONNECTION_STRING']
 
-def query1_createfile(filePath, fileName):
-    blob_service_client = BlobServiceClient.from_connection_string(connectionString)
-    container = blob_service_client.get_container_client("databasesproject")
-    with open(file=os.path.join(filePath, fileName), mode="rb") as data:
-        blob_client = container.upload_blob(name=fileName, data=data, overwrite=True)
-        if blob_client:
-            return True
+class BlobOperations:
+    def __init__(self):
+        self.blob_service_client = BlobServiceClient.from_connection_string(connectionString)
 
-def query3_getbloburl(fileName):
-    blob_service_client = BlobServiceClient.from_connection_string(connectionString)
-    blob_client = blob_service_client.get_blob_client(container="databasesproject", blob=fileName)
-    with open(file=os.path.join(r'', fileName), mode="wb") as sample_blob:
-        download_stream = blob_client.download_blob()
-        sample_blob.write(download_stream.readall())
+    def query1_createfile(self, filePath, fileName):
+        container = self.blob_service_client.get_container_client("databasesproject")
+        with open(file=os.path.join(filePath, fileName), mode="rb") as data:
+            blob_client = container.upload_blob(name=fileName, data=data, overwrite=True)
+            if blob_client:
+                return True
 
-def query4_deleteblob(fileName):
-    blob_service_client = BlobServiceClient.from_connection_string(connectionString)
-    blob_client = blob_service_client.get_blob_client(container="databasesproject", blob=fileName)
-    blob_client.delete_blob()
+    def query3_getbloburl(self, fileName):
+        blob_client = self.blob_service_client.get_blob_client(container="databasesproject", blob=fileName)
+        with open(file=os.path.join(r'', fileName), mode="wb") as sample_blob:
+            download_stream = blob_client.download_blob()
+            sample_blob.write(download_stream.readall())
 
-def query5_editblob(currentFileName, filePath, fileName):
-    blob_service_client = BlobServiceClient.from_connection_string(connectionString)
-    blob_client = blob_service_client.get_blob_client(container="databasesproject", blob=currentFileName)
-    blob_client.delete_blob()
-    blob_client = blob_service_client.get_blob_client(container="databasesproject", blob=fileName)
-    with open(file=os.path.join(filePath, fileName), mode="rb") as data:
-        container = blob_service_client.get_container_client("databasesproject")
-        blob_client = container.upload_blob(name=fileName, data=data, overwrite=True)
+    def query4_deleteblob(self, fileName):
+        blob_client = self.blob_service_client.get_blob_client(container="databasesproject", blob=fileName)
+        blob_client.delete_blob()
 
-if __name__ == "__main__":
-    filePath = ""
-    fileName = "testfile.txt"
-    newFileName = "testfile2.txt"
-    query3_getbloburl(newFileName)
-
+    def query5_editblob(self, currentFileName, filePath, fileName):
+        blob_client = self.blob_service_client.get_blob_client(container="databasesproject", blob=currentFileName)
+        blob_client.delete_blob()
+        blob_client = self.blob_service_client.get_blob_client(container="databasesproject", blob=fileName)
+        with open(file=os.path.join(filePath, fileName), mode="rb") as data:
+            container = self.blob_service_client.get_container_client("databasesproject")
+            blob_client = container.upload_blob(name=fileName, data=data, overwrite=True)
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from influx_cmds import DBInflux
-
+from file_uploading import BlobOperations
 # Example for influx
 influx_client = DBInflux()
+blob_client = BlobOperations()
 user1 = 1
 user2 = 2
 post1 = 1
@@ -14,3 +15,51 @@ influx_client.upvote_post(user1, post1)
 top_posts = influx_client.get_top_n_upvoted_post("7d", n=2)
 for post in top_posts:
     print(f"Post {post['post_id']} has {post['upvotes']} upvotes.")
+
+def createpost(posttitle, postdescription, postuser, postfilepath, postfilename):
+    # insert SQL entry here inserting the post
+
+    post = 1
+    influx_client.create_post(postuser,post)
+    BlobOperations.query1_createfile(postfilepath, postfilename)
+
+def getposts():
+    recent_posts = influx_client.get_top_n_upvoted_post("1d", 10)
+    for post in recent_posts:
+        print(f"Post: {post['post)id']}")
+
+def detailpost(post_id):
+    # SQL Query to get the post id
+
+    #post = 
+    # the syntax below shall be changed once we have the sql object
+    BlobOperations.query3_getbloburl(post.filename)
+
+    ## insert print statements here
+
+def deletepost(post_id, user_id):
+    # SQL Query Here for the object
+    #post = 
+
+    influx_client.delete_post(user_id,post_id)
+    BlobOperations.query4_deleteblob(post.filename)
+
+    # Once these are deleted, delete the sql entry
+    
+    print("Post {post.id} Deleted")
+
+def editpost(post_id, posttitle, postdescription, postuser, postfilepath, postfilename):
+    # sql query to get the current file's name if any
+    # post = 
+
+    if postfilename != None and postfilepath != None:  
+      if post.filename:
+          BlobOperations.query5_editblob(post.filename, postfilepath, postfilename)
+      else:
+          BlobOperations.query1_createfile(postfilepath, postfilename)
+
+def upvote_post(post_id, user_id, vote):
+    # sql logic here
+
+    influx_client.upvote_post(user_id, post_id)
+    
